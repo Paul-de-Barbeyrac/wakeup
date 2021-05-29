4153173823b55a8001725ad5adb230edb5cdccb3
Paul de Barbeyrac <dev.pauldebarbeyrac@gmail.com>
2021-05-29 09:37:07 +0200

    Fix haar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('opencv/haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
