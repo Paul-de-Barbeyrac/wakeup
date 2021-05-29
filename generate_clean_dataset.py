d6a241e994e142c6b3023409497fedccd1fdbde7
Paul de Barbeyrac <dev.pauldebarbeyrac@gmail.com>
2021-05-29 14:57:54 +0200

    Resize all photos and dump for easier loading and processing

---
+++ generate_clean_dataset.py
@@ -0,0 +1,24 @@
+import numpy as np
+from skimage.transform import resize
+import cv2
+import glob
+
+
+def generate_clean_dataset():
+    images = []
+    y = []
+
+    for filepath in glob.iglob('dataset/*/*.png'):
+        img = cv2.imread(filepath)
+        image_resized = resize(img, (28, 28), anti_aliasing=True)
+        if img is not None:  # in case unable to open file
+            images.append(image_resized)
+            y.append(int(filepath.split("_")[4]))  # Retrieve eye state from file naming convention
+
+    # Dump file for faster reuse
+    np.save('dataset_cleaned/eye_images.npy', images)
+    np.save('dataset_cleaned/eye_state.npy', y)
+
+
+if __name__ == '__main__':
+    generate_clean_dataset()
