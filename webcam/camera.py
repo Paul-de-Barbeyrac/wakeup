d912a7b51526e6d2b211782edefb5aad474bc46a
Paul de Barbeyrac <dev.pauldebarbeyrac@gmail.com>
2021-06-03 21:08:26 +0200

    Introduce left and right eye with ok-ish detection but predict not working and FPS too slow

--- webcam/camera.py
+++ webcam/camera.py
@@ -1,7 +1,12 @@
 import cv2
+import numpy as np
+from tensorflow import keras
 
 face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('opencv/haarcascade_eye.xml')
+left_eye_cascade = cv2.CascadeClassifier('opencv/haarcascade_lefteye_2splits.xml')
+right_eye_cascade = cv2.CascadeClassifier('opencv/haarcascade_righteye_2splits.xml')
+
+model_deep = keras.models.load_model("model/model.h5")
 
 
 class VideoCamera(object):
@@ -14,7 +19,7 @@ class VideoCamera(object):
     def get_frame(self):
         tickmark = cv2.getTickCount()
         success, image = self.video.read()
-
+        lbl = "Unknown"
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(
@@ -26,23 +31,48 @@ class VideoCamera(object):
         )
 
         for (x, y, w, h) in faces:
+
             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = image[y:y + h, x:x + w]
 
-            eyes = eye_cascade.detectMultiScale(
+            left_eye = left_eye_cascade.detectMultiScale(
                 roi_gray,
-                scaleFactor=1.05,
-                minNeighbors=6,
+                scaleFactor=1.4,
+                minNeighbors=10,
                 minSize=(60, 60)
             )
 
-            for (ex, ey, ew, eh) in eyes:
+            right_eye = right_eye_cascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=1.4,
+                minNeighbors=10,
+                minSize=(60, 60)
+            )
+
+            for (ex, ey, ew, eh) in left_eye:
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
+
+            for (ex, ey, ew, eh) in right_eye:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+                r_eye = image[y:y + h, x:x + w]
+                r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
+                r_eye = cv2.resize(r_eye, (28, 28))
+                r_eye = r_eye / 255
+                r_eye = r_eye.reshape(28, 28, -1)
+                r_eye = np.expand_dims(r_eye, axis=0)
+                rpred = model_deep.predict_classes(r_eye)
+                if round(rpred[0][0]) == 1:
+                    lbl = 'Open'
+                if round(rpred[0][0]) == 0:
+                    lbl = 'Closed'
+
 
         frame_flip = cv2.flip(image, 1)  # flip vertically
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - tickmark)
         cv2.putText(frame_flip, f"FPS: {round(fps, 1)}", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.putText(frame_flip, f"{lbl}", (40, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
         ret, jpeg = cv2.imencode('.jpg', frame_flip)
 
         return jpeg.tobytes()
