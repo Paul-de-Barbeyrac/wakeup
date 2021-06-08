39cd4479851a024765d518ef4a6916eb79ac84dd
BARBEYRAC (de) Paul <pbarbeyrac@fr.perenco.com>
2021-06-08 20:09:46 +0200

    Image upload module finally working

--- webcam/views.py
+++ webcam/views.py
@@ -1,6 +1,6 @@
 import base64
+from PIL import Image
 from base64 import b64encode
-
 import cv2
 import numpy
 import numpy as np
@@ -9,7 +9,15 @@ from PIL import Image
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from webcam.camera import VideoCamera
-from webcam.form import UploadFileForm
+
+face_cascade = cv2.CascadeClassifier('opencv/haarcascade_frontalface_default.xml')
+
+
+def readb64(base64_string):
+    sbuf = io.BytesIO()
+    sbuf.write(base64.b64decode(base64_string))
+    pimg = Image.open(sbuf)
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 
 def index(request):
@@ -28,29 +36,33 @@ def image(request):
         image_base64 = base64.b64encode(image)
         image_ascii = image_base64.decode('ascii')
         raw_image = f"data:image/jpeg;base64,{image_ascii}"
-        form = UploadFileForm(request.FILES['file'])
 
-        print(raw_image)
+        cvimg = readb64(image_ascii)
+        gray = cv2.cvtColor(cvimg, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.05,
+            # the higher the faster the detection but can be missing detection. Approx range 1.05 - 1.4
+            minNeighbors=6,  # how many neighbors each candidate rectangle should have to retain it. Approx range 3 - 6
+            minSize=(200, 200)  # objects smaller than that dimensions in pixels are ignored. Range depends on objects
+        )
 
-        base64_decoded = base64.b64decode(image_base64)
-        image = Image.open(io.BytesIO(base64_decoded))
-        image_np = np.array(image)
-        print(image_np.shape)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(cvimg, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
-        image_opencv = Image.fromarray(image_np.astype(np.uint8))
-        # Plante ici. A revoir
-        image_opencv_base64 = base64.b64encode(image_opencv)
-        image_opencv_ascii = image_opencv_base64.decode('ascii')
-        processed_image = f"data:image/jpeg;base64,{image_opencv_ascii}"
+        cv2.putText(cvimg, f"TOTO EST LA", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
 
-        print(processed_image)
+        ret, frame_buff = cv2.imencode('.jpg', cvimg)
+        image_base64_cv = base64.b64encode(frame_buff)
+        image_ascii_cv = image_base64_cv.decode('ascii')
+        raw_image_cv = f"data:image/jpeg;base64,{image_ascii_cv}"
+        processed_image = raw_image_cv
 
     else:
-        form = UploadFileForm()
         raw_image = None
 
     return render(request, 'webcam/image.html',
-                  {'form': form, 'raw_image': raw_image, 'processed_image': processed_image})
+                  {'raw_image': raw_image, 'processed_image': processed_image})
 
 
 def gen(camera):
