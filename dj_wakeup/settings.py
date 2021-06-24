30d80cd5280433045e9d64891d4ea12333bd4d3c
Paul de Barbeyrac <dev.pauldebarbeyrac@gmail.com>
2021-06-24 21:36:15 +0200

    ngrok allow all host

--- dj_wakeup/settings.py
+++ dj_wakeup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config.get("GENERAL", 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.get("GENERAL", 'DEBUG')
 
-ALLOWED_HOSTS = [config.get("GENERAL", 'ALLOWED_HOSTS'),'127.0.0.1']
+ALLOWED_HOSTS = [config.get("GENERAL", 'ALLOWED_HOSTS'),'127.0.0.1','*']
 
 # Application definition
 
